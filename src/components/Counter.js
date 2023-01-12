6beb0ba6181f3c5171e41840ecf9ca1f94b9392e
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-01-12 20:40:41 +0300

    Edited the state to use a callback function

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,11 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    setCount((count) => count += 1);
+    console.log(count);
+  // => 0
+
+  
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
